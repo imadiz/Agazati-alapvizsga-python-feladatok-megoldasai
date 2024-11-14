471f2f42d4663cb5f8fa981a5fe0bb0bd7252e45
adiz <weszerle42@gmail.com>
2024-11-14 15:16:39 +0100

    10K doga & misc.

--- vizsgafeladatok_10K.py
+++ vizsgafeladatok_10K.py
@@ -35,3 +35,43 @@ def osszeg(lista):
 
 assert osszeg( [1, 2, 3, 4, 5, 6] ) == 21
 
+    
+def halmazok_metszete(halmaz1, halmaz2):
+    valasz = set()
+    for i in halmaz1:
+        for j in halmaz2:
+            if (i == j):
+                valasz.add(i)
+                
+    return valasz
+# Az assert csak hiba esetén ad visszajelzést.
+
+assert halmazok_metszete({1, 2, 3, 4, 5, 6, 7}, {4, 5, 6, 7, 8, 9}) == {4, 5, 6, 7}
+
+def halmazok_unioja(halmaz1, halmaz2):
+    valasz = set()
+    for x in halmaz1:#Végiglépkedés a halmaz1-en
+        valasz.add(x)#Hozzáadás a halmazhoz (az összes elemet)
+        
+    for x in halmaz2:#Végigmegyek a másik halmazon
+        if(x not in halmaz1):
+            valasz.add(x)
+    return valasz
+# Az assert csak hiba esetén ad visszajelzést.
+
+assert halmazok_unioja({1, 2, 3, 4, 5, 6, 7}, {4, 5, 6, 7, 8, 9}) == {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+def faktorialis(n):
+    valasz = 1
+    for i in range(1, n+1):
+        valasz *= i
+        
+    return valasz    
+        
+
+# Az assert csak hiba esetén ad visszajelzést.
+
+assert  faktorialis(0) == 1
+assert  faktorialis(1) == 1
+assert  faktorialis(2) == 1 * 2
+assert  faktorialis(5) == 1 * 2 * 3 * 4 * 5
